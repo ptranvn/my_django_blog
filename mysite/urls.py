f51593efd6007a0131eb3155ae0c19330aeb1f4d
phutran <ptranvn@gmail.com>
2023-06-07 15:44:00 -0700

    delete rest_api app

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,12 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from django.contrib.auth.views import LoginView, LogoutView
-from rest_framework import routers
-from tutorial.quickstart import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path("", include("blogging.urls")),
@@ -31,6 +26,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
-    path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
 ]
